b1fa7dec9dc6c05411f31876524ac786e5896c46
dbcapprentice <dbcapprentice@dbc.com>
2013-09-05 14:47:51 -0700

    add recipe validations

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -6,6 +6,26 @@ describe Recipe do
     @recipe = FactoryGirl.create :recipe
   end
 
+  describe 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_uniqueness_of :title }
+
+    describe 'image_url' do
+      it { should validate_presence_of :image_url }
+
+      it 'should accept valid urls' do
+        @recipe.image_url = 'http://www.foo.com/foo.jpg'
+        expect(@recipe).to be_valid
+      end
+
+      it 'should not accept invalid urls' do
+        @recipe.image_url = 'www.'
+        expect(@recipe).not_to be_valid
+      end
+    end
+
+  end
+
   subject { @recipe }
 
   it { should respond_to :title }
