42968b6c54b97160c1552425f36f3b040294d76b
dbcapprentice <dbcapprentice@dbc.com>
2013-09-05 12:26:07 -0700

    add recipe factory

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -2,5 +2,9 @@
 
 FactoryGirl.define do
   factory :recipe do
+    sequence :title do
+      |n| "Steak #{n}"
+    end
+    image_url 'http://www.neontommy.com/sites/default/files/uploads/moretonfigsteak.JPG'
   end
 end
