5aaf3f7af59aaf13123b2369dce363d80512fb41
Jeremy Sell <thestereoscopics@gmail.com>
2013-09-05 14:58:22 -0700

    refactor seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-recipe = Recipe.create(title: 'Ketchup, Mustard Dog', image_url: 'http://i-cdn.apartmenttherapy.com/ol-images/la/uploads/092806canstruct1.jpg')
-recipe = Recipe.create(title: 'Beef, Butter and Rosemary', image_url: 'http://thesuiteworld.com/wp-content/uploads/2012/09/mastros-bone-in-ribeye-best-steak-steakhouse.jpg')
-recipe = Recipe.create(title: 'Avocado and Seaweed', image_url: 'http://1.bp.blogspot.com/-DeW1rDMgI1k/UG-aRX_lozI/AAAAAAAACfw/M--XhwbRw_8/s400/bowl.jpg')
-recipe = Recipe.create(title: 'Kevin Bacon', image_url: 'http://laughingsquid.com/wp-content/uploads/Kevin-Bacon.jpg')
-recipe = Recipe.create(title: 'Octopus and Beans', image_url: 'http://kelseats.com/wp-content/uploads/2011/04/Bruschetta-with-Calamari-and-Tomato-Nopa.jpg')
-recipe = Recipe.create(title: 'Bacon Beignets', image_url: 'http://3.bp.blogspot.com/_3h4FWGsx1DE/TEtktnTXWRI/AAAAAAAAB2I/7aa0D9lsOus/s400/bacon+beignets.jpg')
-recipe = Recipe.create(title: 'Beignets Quartet', image_url: 'http://imonlyhereforthefood.com/images/Food/BrendasFrenchSoulFood/BrendasFrenchSoulFood002.jpg')
-recipe = Recipe.create(title: 'Plow Stack', image_url: 'http://s3-media4.ak.yelpcdn.com/bphoto/EA0sTm3UqyMtayQLk08hOQ/l.jpg')
-recipe = Recipe.create(title: 'Cheese and More', image_url: 'http://www.esquire.com/cm/esquire/images/eZ/nopalito-mexican-food-1109-lg.jpg')
-recipe = Recipe.create(title: 'Omelette', image_url: 'http://www.tablehopper.com/newsletter/070417/foreign_cinema_large.jpg')
-recipe = Recipe.create(title: 'Adorable', image_url: 'http://wineormous.com/wp-content/uploads/2012/12/WINEormous-at-The-French-Laundry-in-Yountville-CA.jpg')
+recipe = { 'Ketchup, Mustard Dog' => 'http://i-cdn.apartmenttherapy.com/ol-images/la/uploads/092806canstruct1.jpg',
+           'Beef, Butter and Rosemary' => 'http://thesuiteworld.com/wp-content/uploads/2012/09/mastros-bone-in-ribeye-best-steak-steakhouse.jpg',
+           'Avocado and Seaweed' => 'http://1.bp.blogspot.com/-DeW1rDMgI1k/UG-aRX_lozI/AAAAAAAACfw/M--XhwbRw_8/s400/bowl.jpg',
+           'Kevin Bacon' => 'http://laughingsquid.com/wp-content/uploads/Kevin-Bacon.jpg',
+           'Octopus and Beans' => 'http://kelseats.com/wp-content/uploads/2011/04/Bruschetta-with-Calamari-and-Tomato-Nopa.jpg',
+           'Bacon Beignets' => 'http://3.bp.blogspot.com/_3h4FWGsx1DE/TEtktnTXWRI/AAAAAAAAB2I/7aa0D9lsOus/s400/bacon+beignets.jpg',
+           'Beignets Quartet' => 'http://imonlyhereforthefood.com/images/Food/BrendasFrenchSoulFood/BrendasFrenchSoulFood002.jpg',
+           'Plow Stack' => 'http://s3-media4.ak.yelpcdn.com/bphoto/EA0sTm3UqyMtayQLk08hOQ/l.jpg',
+           'Cheese and More' => 'http://www.esquire.com/cm/esquire/images/eZ/nopalito-mexican-food-1109-lg.jpg',
+           'Omelette' => 'http://www.tablehopper.com/newsletter/070417/foreign_cinema_large.jpg',
+           'Adorable' => 'http://wineormous.com/wp-content/uploads/2012/12/WINEormous-at-The-French-Laundry-in-Yountville-CA.jpg'}
+
+recipe.each { |title, image| Recipe.create(title: title, image_url: image) }
