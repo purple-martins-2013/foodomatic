ae98a69ddbc1c611aae9da9670dee94ae2eac287
= <=>
2013-09-05 15:34:32 -0700

    Create and style recipe index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Foodomatic::Application.routes.draw do
+  root 'main#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
