15f724a83c0bfb56704f849028a633c2b2d38628
Jeremy Sell <thestereoscopics@gmail.com>
2013-09-05 16:41:55 -0700

    Create user seed and devise views for login

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,14 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+  sequence :email do |n| 
+    "email#{n}@factory.com"
+  end 
+
   factory :user do
-  end
+    email 
+    password              { "password" }
+    password_confirmation { "password" }
+  end 
+
 end
