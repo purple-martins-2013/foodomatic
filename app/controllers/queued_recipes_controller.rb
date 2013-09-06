33c8bf97fe0873d73a01dfc91bfd48caf1325443
= <=>
2013-09-06 15:37:26 -0700

    Link to and display a user's queued recipes

--- app/controllers/queued_recipes_controller.rb
+++ app/controllers/queued_recipes_controller.rb
@@ -1,8 +1,12 @@
 class QueuedRecipesController < ApplicationController
   def create
     QueuedRecipe.create(
-      user_id: params[:user_id], 
+      user_id: current_user.id, 
       recipe_id: params[:recipe_id])
     redirect_to recipes_path
+  end
+
+  def index
+    @queued_recipes = current_user.queued_recipes
   end
 end
