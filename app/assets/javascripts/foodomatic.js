080c04897feb48566eaf7e4bf8f3b1a0c8adf90a
Jeremy Sell <thestereoscopics@gmail.com>
2013-09-06 18:26:28 -0700

    Links for bagdges fix and modals if not logged in.

--- app/assets/javascripts/foodomatic.js
+++ app/assets/javascripts/foodomatic.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
-  $('.queue-it').on('click', function(e) {
+  $('.add-to-queue').on('click', function(e) {
     e.preventDefault();
-    $('#queue').effect('bounce', {times: 3}, "slow").badger('+5');
+    $('#queue-link').effect('bounce', {times: 3}, "slow").badger('+5');
+  });
+  $('.add-to-basket').on('click', function(e) {
+    e.preventDefault();
+    $('#basket-link').effect('bounce', {times: 3}, "slow").badger('+5');
   });
 });
