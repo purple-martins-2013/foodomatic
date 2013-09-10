6fd401f7ca0102ecfaa44d3a0a6b0f9a80dcbf73
ltlai <leslie.t.lai@gmail.com>
2013-09-10 11:23:20 -0700

    refactor favorites into own class

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,16 +20,4 @@ class User < ActiveRecord::Base
   def favorites
     @favorites ||= Favorites.new(self)
   end
-
-
-  class Favorites
-    attr_reader :user
-    def initialize(user)
-      @user = user
-    end
-
-    def list
-      user.favorite_recipes.ordered.with_recipe.map(&:recipe)
-    end
-  end
 end
