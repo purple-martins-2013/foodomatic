dad24f73bc1b05ae63b0e1c5a766f7c3e000c7f0
dbcapprentice <dbcapprentice@dbc.com>
2013-09-10 15:49:00 -0700

    ingredient fixes

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,14 +1,24 @@
 class IngredientsController < ApplicationController
 
+  before_action :set_ingredient, only: [:show]
+
   def create
     recipe = Recipe.find_by_id(ingredient_params[:recipe_id])
     ingredient = recipe.ingredients.build(ingredient_params)
     if ingredient.save
-      render json: ingredient.to_json
+      redirect ingredient_path ingredient
     end
   end
 
+  def show
+    render partial: 'ingredient', locals: { ingredient: @ingredient }
+  end
+
   private
+
+  def set_ingredient
+    @ingredient = Ingredient.find(params[:id])
+  end
 
   def ingredient_params
     params.require(:ingredient).permit(:recipe_id, :product_id, :required_amount)
