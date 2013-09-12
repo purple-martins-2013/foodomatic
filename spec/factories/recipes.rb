42f0925f1f2a6468c50b169c8b952c632e815c94
dbcapprentice <dbcapprentice@dbc.com>
2013-09-12 11:17:40 -0700

    better food porn and new name

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -15,6 +15,32 @@ food_porn_urls = [
   'http://i.imgur.com/RhtK0ZY.jpg',
   'http://farm8.staticflickr.com/7319/9716454028_5c1ebfc08d_k.jpg',
   'http://i.imgur.com/MQWF6Re.jpg',
+  'http://i.imgur.com/p4HnQd8.jpg',
+  'http://i.imgur.com/fWzSE.jpg',
+  'http://i.imgur.com/i3kUJ.jpg',
+  'http://i.imgur.com/aG03T.jpg',
+  'http://i.imgur.com/TQ9P03b.jpg',
+  'http://bakecrumbs.files.wordpress.com/2013/07/ccjupitercake08.jpg',
+  'http://i.imgur.com/RiY8Llk.jpg',
+  'http://i.imgur.com/HNKuBK5.jpg',
+  'http://i.imgur.com/D0zIAxC.jpg',
+  'http://i.imgur.com/gU2Bdn2.jpg',
+  'http://i.imgur.com/ZTGqigb.jpg',
+  'http://blog.hostthetoast.com/wp-content/uploads/2013/01/Can-Cakes-0443.jpg',
+  'http://i.imgur.com/6LOAp.jpg',
+  'http://i.imgur.com/0YYEZqp.jpg',
+  'http://i.imgur.com/5dONz.jpg',
+  'http://i.imgur.com/Yrp9nZu.jpg',
+  'http://i.imgur.com/CeeHIIm.jpg',
+  'http://i.imgur.com/EZh81wU.jpg',
+  'http://i.imgur.com/J9DLQ.jpg',
+  'http://i.imgur.com/6moxCeT.jpg',
+  'http://i.imgur.com/C45KqGY.jpg',
+  'http://i.imgur.com/F6S1p.jpg',
+  'http://i.imgur.com/gP3nb6n.jpg',
+  'http://i.imgur.com/avBDsUn.jpg',
+  'http://i.imgur.com/CZLh2tI.jpg',
+  'http://i.imgur.com/8nuolGP.jpg#.UVIfdXvd27k.reddit',
 ]
 
 FactoryGirl.define do
@@ -25,7 +51,7 @@ FactoryGirl.define do
 
     sequence :image_url do |n|
       # "http://placehold.it/500x500&text=Steak_#{n}"
-      food_porn_urls.sample
+      food_porn_urls.shuffle!.pop
     end
 
     factory :recipe_with_ingredients do
