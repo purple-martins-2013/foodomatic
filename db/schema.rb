cf19e7a347a0b1d94652d82bdc3aa1b14270c4fc
Dan Hossom <dan@Dans-MacBook-Pro-2.local>
2013-09-11 20:58:47 -0700

    rails g migration for uid, name, provider

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130910222824) do
+ActiveRecord::Schema.define(version: 20130912035730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,9 @@ ActiveRecord::Schema.define(version: 20130910222824) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
